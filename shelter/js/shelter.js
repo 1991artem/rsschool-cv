1ca8b701614f817677ae1b38b423176ca7ca5f90
1991artem <ar_puz@mail.ru>
2022-03-31 17:29:40 +0300

    fix:"slider"

--- shelter/js/shelter.js
+++ shelter/js/shelter.js
@@ -1,49 +1,47 @@
+let slider = {};
 
-/* Индекс слайда по умолчанию */
-let slideIndex = 1;
-showSlides(slideIndex);
+slider.slideIndex = 0;
 
 /* Функция увеличивает индекс на 1, показывает следующй слайд*/
-function plusSlide() {
-    showSlides(slideIndex += 1);
+slider.plusSlide = function plusSlide() {
+    slider.show(slider.slideIndex += 1);
 }
 
 /* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
-function minusSlide() {
-    showSlides(slideIndex -= 1);  
+slider.minusSlide = function minusSlide() {
+    slider.show(slider.slideIndex -= 1);  
 }
 
 /* Основная функция слайдера */
-function showSlides(n) {
+slider.show = function showSlides(n) {
     let slides = document.getElementsByName("slide");
-    if (n > slides.length-1) {
-      slideIndex = 0
+    if (n >= slides.length-1) {
+        slider.slideIndex = 0
     }
     if (n < 1) {
-        slideIndex = slides.length-1
+        slider.slideIndex = slides.length-1
     }
-    if (slideIndex == 0){
-        slides[0].style.filter = "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))";
-        slides[1].style.filter = "";
-        slides[2].style.filter = "";
-        slides[0].className = "pets_page_slider_card_2";
-        slides[1].className = "pets_page_slider_card_3";
-        slides[2].className = "pets_page_slider_card_1";
+    slides.forEach(element => {
+        element.style.visibility = "hidden";
+    });
+
+        let arrShow = ["pets_page_slider_card_1","pets_page_slider_card_2","pets_page_slider_card_3"];
+        let count = 0;
+        let i = slider.slideIndex;
+    while (count < 3){
+            slides[i].className = arrShow[count];
+            slides[i].style.visibility = "visible";
+            count++;
+            i++;
+            if (i > slides.length-1){
+                i=0;
+            }
     }
-    if (slideIndex == 1){
-        slides[0].style.filter = "";
-        slides[1].style.filter = "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))";
-        slides[2].style.filter = "";
-        slides[0].className = "pets_page_slider_card_1";
-        slides[1].className = "pets_page_slider_card_2";
-        slides[2].className = "pets_page_slider_card_3";
-    }
-    if (slideIndex == 2){
-        slides[0].style.filter = "";
-        slides[1].style.filter = "";
-        slides[2].style.filter = "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))";
-        slides[0].className = "pets_page_slider_card_3";
-        slides[1].className = "pets_page_slider_card_1";
-        slides[2].className = "pets_page_slider_card_2";
-    }
+
 }
+/* Индекс слайда по умолчанию */
+slider.show(slider.slideIndex);
+
+slider.plusSlide();
+
+slider.minusSlide();
